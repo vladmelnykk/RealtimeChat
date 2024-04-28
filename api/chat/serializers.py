78f1e572e9d2276cc552cdee270644ea41ceee6c
Vlad Melnyk <t4wer.lol2@gmail.com>
2024-04-28 11:18:07 +0300

    create signup request

--- api/chat/serializers.py
+++ api/chat/serializers.py
@@ -2,6 +2,29 @@ from rest_framework import serializers
 from .models import User
 
 
+class SignUpSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['username', 'password',
+                  'first_name', 'last_name']
+        extra_kwargs = {'password': {'write_only': True}}
+
+    def create(self, validated_data):
+        username = validated_data['username'].lower()
+        first_name = validated_data['first_name'].lower()
+        last_name = validated_data['last_name'].lower()
+
+        user = User.objects.create(
+            username=username,
+            first_name=first_name,
+            last_name=last_name
+        )
+        password = validated_data['password']
+        user.set_password(password)
+        user.save()
+        return user
+
+
 class UserSerializer(serializers.ModelSerializer):
 
     name = serializers.SerializerMethodField()
