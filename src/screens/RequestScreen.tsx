aad3259895316b56c4c54842cca18cb2bb1b08c1
Vlad Melnyk <t4wer.lol2@gmail.com>
2024-06-21 22:51:41 +0300

    remove refreshControl

--- src/screens/RequestScreen.tsx
+++ src/screens/RequestScreen.tsx
@@ -1,22 +1,11 @@
 import React from 'react';
-import {
-  ActivityIndicator,
-  FlatList,
-  RefreshControl,
-  ScrollView,
-  StyleSheet,
-  Text,
-  View,
-} from 'react-native';
+import {ActivityIndicator, FlatList, StyleSheet, View} from 'react-native';
 import useStore from '../core/store/store';
 import RequestRow from '../components/RequestComponents/RequestRow';
 import Empty from '../components/Empty';
 
 const RequestScreen = () => {
   const requestList = useStore(state => state.requestList);
-  const setRequestList = useStore(state => state.setRequestList);
-  const socket = useStore(state => state.socket);
-  let [isFetching, setIsFetching] = React.useState(false);
 
   if (requestList === null) {
     return <ActivityIndicator size="large" style={{flex: 1}} />;
@@ -31,18 +20,6 @@ const RequestScreen = () => {
       <FlatList
         ListEmptyComponent={
           <Empty icon={'bell'} message="No requests" centered={true} />
-        }
-        refreshControl={
-          <RefreshControl
-            refreshing={isFetching}
-            onRefresh={() => {
-              socket?.send(
-                JSON.stringify({
-                  source: 'request.list',
-                }),
-              );
-            }}
-          />
         }
         keyExtractor={item => item.sender.username}
         data={requestList}
